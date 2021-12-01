f06cea1b0c076f6ccdedbeeda272154f3b720adc
Megamxl <maximilian.nowak@outlook.at>
2021-12-01 10:15:40 +0100

    habe herausgefundende das aoc 2 parts pro tag hat

--- day1/day1.py
+++ day1/day1.py
@@ -1,7 +1,30 @@
-f = open("C:/Users/gerha/Desktop/aoc 2021/day1/input.txt", "r")
-entries = [int(e) for e in set(f.read().split("\n"))]
+#f = open("C:/Users/gerha/Desktop/aoc 2021/day1/input.txt", "r")
+#lines = [int(e) for e in set(f.read().split("\n"))]
 
-for e in entries:
-    print(e)
+from typing import List
 
-print("git")
+
+lines = []
+with open("C:/Users/gerha/Desktop/aoc 2021/day1/input.txt") as f:
+    lines = f.readlines()
+    
+def partOne(lines):
+    cont = 0
+    for i in range(len(lines)):
+        if i >= 1:
+            if int(lines[i]) > int(lines[i-1]):
+                cont += 1
+    print(" Ergebnis ist " +  str(cont))
+    
+def partTow(list):
+    cont = 0
+    summedUp = []
+    for i in range(len(list)):
+        if i >= 2:
+            summedUp.append(int(list[i]) + int(lines[i-1]) + int(lines[i-2]))
+    partOne(summedUp)
+
+print("part 1 Lösung ")
+partOne(lines)
+print("part 2 Lösung ")
+partTow(lines)
