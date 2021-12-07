c103e63f6a35bc51a7e7ecb2efae0a7efc6de4e1
Megamxl <maximilian.nowak@outlook.at>
2021-12-07 23:56:49 +0100

    addad git ignore

---
+++ day 8/day8.py
@@ -0,0 +1,14 @@
+
+with open("day8\input.txt") as puzzle_input:
+    puzzle_input = [line.rstrip('\n').split(",") for line in puzzle_input.readlines()]
+   
+def partOne(lines):
+    print("hey")
+            
+def partTow(lines):
+    print("hey")
+
+if __name__ == '__main__':
+    partOne(puzzle_input)
+    partTow(puzzle_input)
+    
