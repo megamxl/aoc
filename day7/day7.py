48e1f5eb547e2431f65b412b5e57f8ff8bcfbc67
Megamxl <maximilian.nowak@outlook.at>
2021-12-07 10:47:19 +0100

    solved day 7

---
+++ day7/day7.py
@@ -0,0 +1,32 @@
+
+with open("day7\input.txt") as puzzle_input:
+    puzzle_input = [line.rstrip('\n').split(",") for line in puzzle_input.readlines()]
+    newInput =[]
+    for i in range (len(puzzle_input[0])):
+        newInput.append(int(puzzle_input[0][i])) 
+    
+def partOne(lines):
+    lines.sort()
+    sol=[]
+    for i in lines:
+        x=0
+        for t in lines: 
+            x +=  abs((i- t))
+        sol.append(x)
+    print(min(sol))
+            
+    
+                
+
+def partTow(lines):
+    length_ofLines = len(lines)
+    r = sum(lines) // length_ofLines
+    lambdaAgain = lambda x: x * (x+1) // 2
+    part2 = sum(lambdaAgain(abs(x-r)) for x in lines)
+    print(part2)
+
+    
+if __name__ == '__main__':
+    partOne(newInput)
+    partTow(newInput)
+    
