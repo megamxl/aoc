aa3d9ad1b0259d2091d7fdadbb9f705888151a20
megamxl <maximiliann.nowak@outlook.at>
2021-12-25 18:39:40 +0100

    solved day 10

--- day10/day10.py
+++ day10/day10.py
@@ -3,16 +3,47 @@ with open("day10\input.txt") as puzzle_input:
     puzzle_input = [[nums.split(" ") for nums in line.rstrip("\n").split(" | ") ]for line in puzzle_input.readlines()]
     #print(puzzle_input)
 
+isOpening = lambda x: True if x == '(' or x == '[' or x == '{' or x == '<' else False
    
 def partOne(lines): 
-   for line in lines:
-          for second in line:
-            if len()
-
+    
+    closingPair ={
+        ")" : "(",
+        "]" : "[",
+        "}" : "{",
+        ">" : "<"
+    }
+    
+    wrongChars = []
+     
+    for line in lines:
+        for second in line:
+            for intervals in second:
+                opening =[]
+                for chars in intervals:
+                    if(isOpening(chars)):
+                        opening.append(chars)
+                    else:
+                        lastOpening= opening.pop()
+                        if(closingPair[chars] != lastOpening):
+                            wrongChars.append(chars)
+                            break
+    count =0
+    for illCahar in wrongChars:
+        if(illCahar == ")"):
+            count +=3
+        elif(illCahar == "]"):
+            count +=57
+        elif(illCahar == "}"): 
+            count+= 1197
+        elif(illCahar == ">"):
+            count += 25137
+    return count
+                
 def partTow(lines, puz):
    pass
 
 if __name__ == '__main__':
-    partOne(puzzle_input)
+    print(f"part one {partOne(puzzle_input)} ")
     #partTow(puzzle_input)
     pass
